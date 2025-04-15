435ab6b5b8908528ce05b47e637ea8f732876b7d
Divya-Devendrasingh <148423949+Divya-Devendrasingh@users.noreply.github.com>
2025-04-15 18:13:40 +0530

    Update data_processing.py

--- data_processing.py
+++ data_processing.py
@@ -2,3 +2,16 @@ print("adding all the data processing code for fraud detection")
 
 
 Demo for git project
+
+
+
+print("adding all ml related model")
+
+def model1():
+    print(adding code for ml_model1)
+
+def model2():
+    print(adding code for ml_model2)
+
+def model3():
+    print(adding code for ml_model3)
